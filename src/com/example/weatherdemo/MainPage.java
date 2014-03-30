3139e6cd733b91a5a488a521895eb8d50047b6f3
Steven Xu <gazeing@gmail.com>
2014-03-31 08:36:04 +1100

    update

--- src/com/example/weatherdemo/MainPage.java
+++ src/com/example/weatherdemo/MainPage.java
@@ -1,10 +1,27 @@
 package com.example.weatherdemo;
 
+import android.support.v7.app.ActionBar;
+
+
+
 public class MainPage extends AppPage{
 
 	
 	protected MainPage() {
 		super(R.layout.page_iweather);
 		
+	}
+	
+	@Override
+	protected void onCreate() {
+		
+		super.onCreate();
+
+		ActionBar actionBar = getActionBar();
+		if(actionBar!=null){
+			int i =0;
+			i++;
+		}
+
 	}
 }
