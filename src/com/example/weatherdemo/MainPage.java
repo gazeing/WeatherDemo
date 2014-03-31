5f5f63d38294609c83e8b379e3e03e329db47fca
Steven Xu <gazeing@gmail.com>
2014-04-01 08:30:34 +1100

    update

--- src/com/example/weatherdemo/MainPage.java
+++ src/com/example/weatherdemo/MainPage.java
@@ -1,6 +1,8 @@
 package com.example.weatherdemo;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 
@@ -41,6 +43,72 @@ public class MainPage extends AppPage{
 			svg.setLeftView(R.layout.left_navigation);
 			svg.setRightView(R.layout.right_menu);
 		}
+		
+		if(leftView != null){
+			View menu_settingView = leftView.findViewById(R.id.setting);
+			View menu_skinsView = leftView.findViewById(R.id.skins);
+			View menu_voteView = leftView.findViewById(R.id.vote);
+			View menu_feedbackView = leftView.findViewById(R.id.feedback);
+			View menu_donateView = leftView.findViewById(R.id.donateApp);
+			
+			addOnClick(menu_settingView);
+			addOnClick(menu_skinsView);
+			addOnClick(menu_voteView);
+			addOnClick(menu_feedbackView);
+			addOnClick(menu_donateView);
+		}
 
+	}
+	
+	
+	
+	@Override
+	protected void onViewClick(View aView, int nViewId) {
+		// TODO Auto-generated method stub
+		super.onViewClick(aView, nViewId);
+		
+		switch(nViewId) 
+		{
+		case R.id.setting:
+			{
+				// Start input activity.
+				// Save the info.
+				Log.d("test","button1");
+				
+//				InputFactory.startInput(null, this);
+			}
+			break;
+			
+		case R.id.skins:
+		{
+			// Start history activity.
+			// Save the info.
+			Bundle pBundle = new Bundle();
+			// Show main page.
+			this.postEvent(AppConfig.KHistory, pBundle);
+		}
+		break;
+		
+		
+		case R.id.vote:
+		{
+			// Start generate activity.
+			// Save the info.
+			Bundle pBundle = new Bundle();
+			// Show main page.
+			this.postEvent(AppConfig.KGenerate, pBundle);
+		}
+		break;
+		
+		case R.id.donateApp:
+		{
+			// Start dividends activity.
+			// Save the info.
+			Bundle pBundle = new Bundle();
+			// Show main page.
+			this.postEvent(AppConfig.KDividend, pBundle);
+		}
+		break;
+		}
 	}
 }
