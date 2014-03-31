99c81d7b3bed54d510e3120a8961f0f8d0c8405f
Steven Xu <gazeing@gmail.com>
2014-03-31 15:39:02 +1100

    update

--- src/com/example/weatherdemo/MainPage.java
+++ src/com/example/weatherdemo/MainPage.java
@@ -1,10 +1,17 @@
 package com.example.weatherdemo;
 
-import android.support.v7.app.ActionBar;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.example.weatherdemo.views.SlideViewGroup;
 
 
 
 public class MainPage extends AppPage{
+	
+	
+	View leftView, rightView, mainView;
 
 	
 	protected MainPage() {
@@ -17,10 +24,21 @@ public class MainPage extends AppPage{
 		
 		super.onCreate();
 
-		ActionBar actionBar = getActionBar();
-		if(actionBar!=null){
-			int i =0;
-			i++;
+//		ActionBar actionBar = getActionBar();
+//		if(actionBar!=null){
+//			int i =0;
+//			i++;
+//		}
+		
+		SlideViewGroup svg = (SlideViewGroup) findViewById(R.id.home_slidegroup);
+		if(svg != null){
+			leftView = ((LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.left_navigation, null);
+			rightView = ((LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.right_menu, null);
+			mainView = ((LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.middle_main_page, null);
+			
+			svg.setMainView(R.layout.middle_main_page);
+			svg.setLeftView(R.layout.left_navigation);
+			svg.setRightView(R.layout.right_menu);
 		}
 
 	}
